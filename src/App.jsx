eba5fa24307bfa4c3d5724883f7ca9aa6ae4f1a5
DoelJavid <jdryerson@att.net>
2025-07-27 15:18:47 -0400

    feat: Added playlist and track state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
+import {useState} from "react";
 import Tracklist from "./components/Tracklist";
+import Playlist from "./components/Playlist";
 import "./App.css";
 
 /**
@@ -7,44 +9,46 @@ import "./App.css";
   @import {JSX} from "react"
 */
 
-// Hardcoded array of tracks to test the track list.
-const testTracks = [
-  {
-    name: "Albuquerque",
-    artist: "\"Weird Al\" Yankovic",
-    album: "Running With Scissors",
-    id: "2Ec0liSMY2h4XORHyzKCmY"
-  },
-  {
-    name: "Everything You Know Is Wrong",
-    artist: "\"Weird Al\" Yankovic",
-    album: "Bad Hair Day",
-    id: "3bEIbBCkFaqzvtWjKdQ74c"
-  },
-  {
-    name: "Polka Face",
-    artist: "\"Weird Al\" Yankovic",
-    album: "Alpocalypse",
-    id: "2ELqO1C2U6vhg1BfYp7QhG"
-  },
-  {
-    name: "White & Nerdy",
-    artist: "\"Weird Al\" Yankovic",
-    album: "Straight Outta Lynwood",
-    id: "60R2v9lheAu3lwZwAFxMZK"
-  }
-];
-
 /**
   The base container component for the application.
 
   @returns {JSX.Element}
 */
 function App() {
+  // Hardcoded array of tracks to test the track list.
+  const [tracklist, setTracklist] = useState([
+    {
+      name: "Albuquerque",
+      artist: "\"Weird Al\" Yankovic",
+      album: "Running With Scissors",
+      id: "2Ec0liSMY2h4XORHyzKCmY"
+    },
+    {
+      name: "Everything You Know Is Wrong",
+      artist: "\"Weird Al\" Yankovic",
+      album: "Bad Hair Day",
+      id: "3bEIbBCkFaqzvtWjKdQ74c"
+    },
+    {
+      name: "Polka Face",
+      artist: "\"Weird Al\" Yankovic",
+      album: "Alpocalypse",
+      id: "2ELqO1C2U6vhg1BfYp7QhG"
+    },
+    {
+      name: "White & Nerdy",
+      artist: "\"Weird Al\" Yankovic",
+      album: "Straight Outta Lynwood",
+      id: "60R2v9lheAu3lwZwAFxMZK"
+    }
+  ]);
+  const [playlist, setPlaylist] = useState([]);
+
   return (
-    <>
-      <Tracklist tracks={testTracks} />
-    </>
+    <div>
+      <Tracklist tracks={tracklist} />
+      <Playlist tracks={playlist} />
+    </div>
   );
 }
 
--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,13 +1,38 @@
 
+import Track from "./Track";
+
+/**
+  @import {JSX} from "react"
+  @import {Track} from "./Track"
+*/
+
 /**
   Playlist component to contain all tracks on the user's playlist.
 
+  @param {Object} props
+  @param {(Track | Track[])?} props.tracks
   @returns {JSX.Element}
 */
-function Playlist() {
+function Playlist({tracks}) {
+  let playlist = tracks;
+  if (!(playlist instanceof Array)) {
+    playlist = playlist ? [playlist] : [];
+  }
+
   return (
-    <>
-    </>
+    <div className="playlist">
+      <h2>Playlist Title</h2>
+
+      <ul>
+        {playlist.map((track) => (
+          <li key={`playlist_${track.artist}_${track.id}`}>
+            <Track name={track.name} artist={track.artist} inPlaylist={true} />
+          </li>
+        ))}
+      </ul>
+
+      <button>Save to Spotify</button>
+    </div>
   );
 }
 
